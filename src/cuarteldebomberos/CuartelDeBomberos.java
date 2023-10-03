254ec84a2c753f4817774206d5bd6ae8756bdad6
JohnDeSouza49 <141689720+JohnDeSouza49@users.noreply.github.com>
2023-10-03 12:01:12 -0300

    jjjj

--- src/cuarteldebomberos/CuartelDeBomberos.java
+++ src/cuarteldebomberos/CuartelDeBomberos.java
@@ -14,6 +14,6 @@ public class CuartelDeBomberos {
     public static void main(String[] args) {
         Connection con = Conexion.getConexion();
     }
-  // Bombero bob = new Bombero ("24359684","Andres Gimenez",LocalDate.of(1980, Month.MARCH, 15),"1125897648",12,true);
+    // Bombero bob = new Bombero ("24359684","Andres Gimenez",LocalDate.of(1980, Month.MARCH, 15),"1125897648",12,true);
     }
    
