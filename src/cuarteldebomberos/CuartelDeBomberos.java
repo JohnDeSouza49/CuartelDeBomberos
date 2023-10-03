a9f34b1960949e03bb45ae20e11f4d6f62d1f173
JazminLuque <luque6170@gmail.com>
2023-10-03 20:43:55 -0300

    brigada data

--- src/cuarteldebomberos/CuartelDeBomberos.java
+++ src/cuarteldebomberos/CuartelDeBomberos.java
@@ -1,9 +1,11 @@
 
 package cuarteldebomberos;
 import AccesoADatos.BomberoData;
+import AccesoADatos.BrigadaData;
 import java.time.LocalDate;
 import AccesoADatos.Conexion;
 import Entidades.Bombero;
+import Entidades.Brigada;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.time.Month;
@@ -15,9 +17,14 @@ public class CuartelDeBomberos {
     public static void main(String[] args) {
         Connection con = Conexion.getConexion();
     
-    Bombero bob = new Bombero(25896314, "Anibal Gimenez", LocalDate.of(1980, 3, 15), 1125896325, "AB RH+", 12, true);
-   BomberoData bd = new BomberoData();
-   bd.guardarBombero(bob);
+    //Bombero bob = new Bombero(25896314, "Anibal Gimenez", LocalDate.of(1980, 3, 15), 1125896325, "AB RH+", 12, true);
+   //BomberoData bd = new BomberoData();
+   //bd.guardarBombero(bob);
+   BrigadaData bd= new BrigadaData();
+   //Brigada nueva= new Brigada("Omega", "salvamento en derrumbes", false, 1);
+   //bd.guardarBrigada(nueva);
+        System.out.println(bd.brigadasLibres());
+        System.out.println(bd.brigadasAsignadas());
     } 
 }
 
