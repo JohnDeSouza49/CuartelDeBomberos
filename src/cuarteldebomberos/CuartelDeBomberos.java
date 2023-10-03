2e7a5f640ffe8c80278a6cffc7aba0abe22b80ff
MarcelaCerrudo <140634523+MarcelaCerrudo@users.noreply.github.com>
2023-10-03 20:34:20 -0300

    main probado CrearCuartel

--- src/cuarteldebomberos/CuartelDeBomberos.java
+++ src/cuarteldebomberos/CuartelDeBomberos.java
@@ -3,7 +3,9 @@ package cuarteldebomberos;
 import AccesoADatos.BomberoData;
 import java.time.LocalDate;
 import AccesoADatos.Conexion;
+import AccesoADatos.CuartelData;
 import Entidades.Bombero;
+import Entidades.Cuartel;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.time.Month;
@@ -15,9 +17,12 @@ public class CuartelDeBomberos {
     public static void main(String[] args) {
         Connection con = Conexion.getConexion();
     
-    Bombero bob = new Bombero(25896314, "Anibal Gimenez", LocalDate.of(1980, 3, 15), 1125896325, "AB RH+", 1, true);
-   BomberoData bd = new BomberoData();
-   bd.guardarBombero(bob);
+    //Bombero bob = new Bombero(25896314, "Anibal Gimenez", LocalDate.of(1980, 3, 15), 1125896325, "AB RH+", 1, true);
+  // BomberoData bd = new BomberoData();
+  // bd.guardarBombero(bob);
+  Cuartel c2 = new Cuartel("Cuartel2","Calle2" , 4, 5, 1146668951, "cuartel2@gmail.com");
+  CuartelData cu2 = new CuartelData();
+  cu2.crearCuartel(c2);
     } 
 }
 
