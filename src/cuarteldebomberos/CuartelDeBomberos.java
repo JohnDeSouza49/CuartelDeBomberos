181d5aebc905265f280e9455d2f1f034c1e51e07
MarcelaCerrudo <140634523+MarcelaCerrudo@users.noreply.github.com>
2023-10-03 21:00:33 -0300

    main

--- src/cuarteldebomberos/CuartelDeBomberos.java
+++ src/cuarteldebomberos/CuartelDeBomberos.java
@@ -4,8 +4,10 @@ import AccesoADatos.BomberoData;
 import AccesoADatos.BrigadaData;
 import java.time.LocalDate;
 import AccesoADatos.Conexion;
+import AccesoADatos.CuartelData;
 import Entidades.Bombero;
 import Entidades.Brigada;
+import Entidades.Cuartel;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.time.Month;
@@ -17,14 +19,21 @@ public class CuartelDeBomberos {
     public static void main(String[] args) {
         Connection con = Conexion.getConexion();
     
-    //Bombero bob = new Bombero(25896314, "Anibal Gimenez", LocalDate.of(1980, 3, 15), 1125896325, "AB RH+", 12, true);
+    
+
+
+
+//Bombero bob = new Bombero(25896314, "Anibal Gimenez", LocalDate.of(1980, 3, 15), 1125896325, "AB RH+", 12, true);
    //BomberoData bd = new BomberoData();
    //bd.guardarBombero(bob);
-   BrigadaData bd= new BrigadaData();
+   //BrigadaData bd= new BrigadaData();
    //Brigada nueva= new Brigada("Omega", "salvamento en derrumbes", false, 1);
    //bd.guardarBrigada(nueva);
-        System.out.println(bd.brigadasLibres());
-        System.out.println(bd.brigadasAsignadas());
+       // System.out.println(bd.brigadasLibres());
+       // System.out.println(bd.brigadasAsignadas());
+        Cuartel c2 = new Cuartel("Cuartel2","Calle2" , 4, 5, 1146668951, "cuartel2@gmail.com");
+  CuartelData cu2 = new CuartelData();
+  cu2.crearCuartel(c2);
     } 
 }
 
